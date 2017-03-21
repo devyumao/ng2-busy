b76fe89903cbded8371c509c99a0ef65c0a280b5
IT&feel CA <hola@itandfeel.com>
2017-03-21 17:03:56 -0400

    Temporal solution
    Temporal solution for compiling in issue Error: ngModule should be
    provided

--- build/src/busy.module.d.ts
+++ build/src/busy.module.d.ts
@@ -1,5 +1,6 @@
-import { ModuleWithProviders } from '@angular/core';
-import { BusyConfig } from './busy-config';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {BusyConfig} from './busy-config';
+@NgModule()
 export declare class BusyModule {
     static forRoot(config: BusyConfig): ModuleWithProviders;
 }
